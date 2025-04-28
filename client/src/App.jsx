032b84ff21a5271d679e5d36ac5527c3523af43c
Krost-t <christ.martsanganzoulou@gmail.com>
2025-04-28 18:07:05 +0200

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home     from './pages/Home';
-import Auth     from './pages/Auth';
-import Chatbot from "./pages/ChatBot"
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Chatbot from "./pages/ChatBot";
+
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"      element={<Home />} />
-        <Route path="/auth"  element={<Auth />} />
-        <Route
-          path="/chat"
-          element={
-            <ProtectedRoute>
-              <Chatbot />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*"      element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+ return (
+  <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/auth" element={<Auth />} />
+    <Route path="/chat" element={<Chatbot />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+   </Routes>
+  </BrowserRouter>
+ );
 }
